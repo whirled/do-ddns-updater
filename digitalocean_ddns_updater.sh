e91b47f7d43c271cb312e17fbfcad66a63238a66
William <rettmann@scangroup.net>
2023-11-07 12:17:08 -0600

    Update verbiage in output

--- digitalocean_ddns_updater.sh
+++ digitalocean_ddns_updater.sh
@@ -9,7 +9,7 @@ if [ $# -gt 0 ]; then
     exit 0
 fi
 
-# Get Current IP for FQDN
+# Get Current IP for FQDN from DO DNS
 DNS_IPV4=$(/usr/bin/dig +short @ns1.digitalocean.com "${DO_DNS_FQDN}")
 
 # Get Public IP
@@ -17,7 +17,7 @@ PUBLIC_IPV4=$(/usr/bin/curl -s https://ifconfig.co/)
 
 # Compare DNS IP to Public IP
 if [ "$DNS_IPV4" != "$PUBLIC_IPV4" ]; then
-    echo "Updating ${DO_DNS_FQDN} (ID:${DO_DNS_RECORD_ID}) with IP ${PUBLIC_IPV4}"
+    echo "Public (${PUBLIC_IPV4}) <> DNS (${DNS_IPV4}) - Setting ${DO_DNS_FQDN} = ${PUBLIC_IPV4} (ID:${DO_DNS_RECORD_ID})"
     /usr/bin/curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer ${DO_API_TOKEN}" -d '{"data":"'"${PUBLIC_IPV4}"'"}' "https://api.digitalocean.com/v2/domains/${DO_DNS_DOMAIN}/records/${DO_DNS_RECORD_ID}"
 else
     echo "Public (${PUBLIC_IPV4}) == DNS (${DNS_IPV4}) - No Change"
