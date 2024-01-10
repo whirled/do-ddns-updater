134d95116ef159ef16b1a3ebda76d792ea757a62
William <rettmann@scangroup.net>
2024-01-09 22:32:02 -0600

    Handle an IP not being set due to an error

--- app/digitalocean_ddns_updater.sh
+++ app/digitalocean_ddns_updater.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Treat unset variables and parameters other than the special parameters ‘@’ or ‘*’,
+# or array variables subscripted with ‘@’ or ‘*’, as an error when performing
+# parameter expansion. An error message will be written to the standard error, and
+# a non-interactive shell will exit.
+set -u
+
 # Load Config
 source .env
 
@@ -16,7 +22,9 @@ DNS_IPV4=$(/usr/bin/curl -s -X GET -H "Content-Type: application/json" -H "Autho
 PUBLIC_IPV4=$(/usr/bin/curl -s https://ifconfig.co/)
 
 # Compare DNS IP to Public IP
-if [ "$DNS_IPV4" != "$PUBLIC_IPV4" ]; then
+if [ -z "$DNS_IPV4" ] || [ -z "$PUBLIC_IPV4" ]; then
+    echo "Public (${PUBLIC_IPV4}) ?? DNS (${DNS_IPV4}) - Missing IP - No Change"
+elif [ "$DNS_IPV4" != "$PUBLIC_IPV4" ]; then
     echo "Public (${PUBLIC_IPV4}) <> DNS (${DNS_IPV4}) - Setting ${DO_DNS_FQDN} = ${PUBLIC_IPV4} (ID:${DO_DNS_RECORD_ID})"
     /usr/bin/curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer ${DO_API_TOKEN}" -d '{"data":"'"${PUBLIC_IPV4}"'"}' "https://api.digitalocean.com/v2/domains/${DO_DNS_DOMAIN}/records/${DO_DNS_RECORD_ID}"
 else
